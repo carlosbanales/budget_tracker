114b82652f91f9cfd8978f84abdd4ed60a94c6ad
carlosbanales <carlos@7sixtymedia.com>
2022-02-01 22:44:38 -0800

    made List component

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -4,11 +4,41 @@ import { TextField, Typography, Grid, Button, FormControl, InputLabel, Select, M
 import useStyles from './styles';
 
 const Form = () => {
-  const classes = useStyles();
-  return (
-    <div>
-
-    </div>
-  )};
+    const classes = useStyles();
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={12}>
+                <Typography align="center" variant="subtitle2" gutterbottom>
+                ...
+                </Typography>
+            </Grid>
+            <Grid item xs={6}>
+                <FormControl fullWidth>
+                    <InputLabel>Type</InputLabel>
+                    <Select>
+                        <MenuItem value="Income">Income</MenuItem>
+                        <MenuItem value="Expense">Expense</MenuItem>
+                    </Select>
+                </FormControl>
+            </Grid>
+            <Grid item xs={6}>
+                <FormControl fullWidth>
+                    <InputLabel>Category</InputLabel>
+                    <Select>
+                        <MenuItem value="Business">Business</MenuItem>
+                        <MenuItem value="salary">Salary</MenuItem>
+                    </Select>
+                </FormControl>
+            </Grid>
+            <Grid item xs={6}>
+                <TextField type="number" label="amount" fullWidth />
+            </Grid>
+            <Grid item xs={6}>
+                <TextField type="date" label="Date" fullWidth />
+            </Grid>
+            <Button className={classes.button} variant="outlined" color="primary fullWidth">Create</Button>
+        </Grid>
+    )
+};
 
 export default Form;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
 
 import useStyles from './styles';
+import Form from './Form/Form';
 
 const Main = () => {
 	const classes = useStyles();
@@ -16,7 +17,7 @@ const Main = () => {
 					Try Saying: Add income for $100 in Catagory salary for Monday...
 				</Typography>
 				<Divider />
-				{ /* Form */ }
+				<Form />
 			</CardContent>
 			<CardContent className={classes.CardContent}>
 				<Grid container spacing={2}>
