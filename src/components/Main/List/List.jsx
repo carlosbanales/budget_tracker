b0480eab36c5adeebd544f9704dccf01ce18804e
carlosbanales <carlos@7sixtymedia.com>
2022-02-01 23:09:43 -0800

    Added List component

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { List as MUIList, ListItem, ListItemAvatar, ListItemtext, Avatar, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core';
+import { Delete, MoneyOff } from '@material-ui/icons';
+
+import useStyles from './styles';
 
 const List = () => {
+    const classes = useStyles();
+    
+    const transactions = [
+        { id: 1, type: "Income"}
+    ];
+
     return(
-        <div></div>
-    )
+        <MUIList dense={false} className={classes.list}>
+            {transaction.map((transaction) => (
+                <Slide direction="down" in mountOnEnter unmountOnExit key={transaction.id}>
+                    <ListItem>
+                        <ListItemAvatar>
+                            <Avatar className={transaction.type === 'Income' ? classes.avatarIncome : classes.avatarExpense}>
+                                <MoneyOff />
+                            </Avatar>
+                        </ListItemAvatar>
+                     </ListItem>
+                </Slide>
+            ))}
+        </MUIList>
+    
 };
 
 export default List;
